2dd32469f3d5b166abd871cea1ca7d3c54c054c8
v.shkerdin <v.shkerdin@crpt.ru>
2020-02-24 21:55:09 +0300

    Begin

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className={'header'}>
+        <img src={"https://tinyjpg.com/images/social/website.jpg"}  alt={""} />
+    </header>
+};
+
+export default Header;
