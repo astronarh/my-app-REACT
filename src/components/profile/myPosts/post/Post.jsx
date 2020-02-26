5e6057c2a17a2285b1ae5907d97db226a759127b
v.shkerdin <v.shkerdin@crpt.ru>
2020-02-26 17:50:23 +0300

    17

--- src/components/profile/myPosts/post/Post.jsx
+++ src/components/profile/myPosts/post/Post.jsx
@@ -4,7 +4,8 @@ import s from './Post.module.css'
 const Post = (props) => {
     return (
         <div className={s.item}>
-            <img src={'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP0149-CUSA09988_00-AV00000000000004/image?w=960&h=960&bg_color=000000&opacity=100&_version=00_09_000'}/>
+            <img
+                src={'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/19/EP0149-CUSA09988_00-AV00000000000004/image?w=960&h=960&bg_color=000000&opacity=100&_version=00_09_000'}/>
             {props.message}
             <div>
                 <span>like: {props.likeCount}</span>
