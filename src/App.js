2aaee1359520e7303c030f1f9c093d21bc465823
v.shkerdin <v.shkerdin@crpt.ru>
2020-02-26 21:17:48 +0300

    18

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
+import Dialogs from "./components/dialogs/Dialogs";
 
 class App extends React.Component {
-  render = () => <div className={'app-wrapper'}>
-    <Header/>
-    <Navbar/>
-    <Profile/>
-  </div>;
+    render = () => <div className={'app-wrapper'}>
+        <Header/>
+        <Navbar/>
+        {/*<Profile/>*/}
+        <div className={'app-wrapper-content'}>
+          <Dialogs/>
+        </div>
+    </div>;
 }
 
 export default App;
