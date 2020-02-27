4d3ad904653457fc5a68cdd81cb8a03928930bac
v.shkerdin <v.shkerdin@crpt.ru>
2020-02-27 21:01:24 +0300

    21

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dialog 1
+                </div>
+                <div className={s.dialog}>
+                    Dialog 2
+                </div>
+                <div className={s.dialog}>
+                    Dialog 3
+                </div>
+                <div className={s.dialog}>
+                    Dialog 4
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hi Hi</div>
+                <div className={s.message}>Hi Hi Hi</div>
+            </div>
         </div>
     )
 };
