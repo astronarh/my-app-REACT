fc55ca517188307720e711610e95508dd42be774
v.shkerdin <v.shkerdin@crpt.ru>
2020-03-02 15:53:58 +0300

    33 (add empty test)

--- src/render.jsx
+++ src/render.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {addPost} from "./redax/state";
+
 import {BrowserRouter} from "react-router-dom";
 
 export let rerendetEntireTree = (state) => {
