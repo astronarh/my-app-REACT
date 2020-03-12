bc3bc1c498709fe571b9d30fcb612c0f40f72a2d
v.shkerdin <v.shkerdin@crpt.ru>
2020-03-12 15:23:48 +0300

    57

---
+++ src/components/common/preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "../../../assets/images/preloader.gif";
+import React from "react";
+
+let Preloader = (props) => {
+    return <img width={100} src={preloader}/>
+};
+
+export default Preloader;
