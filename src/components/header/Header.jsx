ff3cadbaf8679ccca9405112bc875a71c27939e8
v.shkerdin <v.shkerdin@crpt.ru>
2020-03-14 22:28:48 +0300

    61

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-        <img src={"https://tinyjpg.com/images/social/website.jpg"} alt={""} />
+        <img src={"https://tinyjpg.com/images/social/website.jpg"} alt={""}/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ?
+                props.login :
+                <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 };
 
