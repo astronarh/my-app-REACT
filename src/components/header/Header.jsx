039dea21ce05faa7fb0d8237f882332772ee4d84
v.shkerdin <v.shkerdin@crpt.ru>
2020-03-31 09:57:31 +0300

    78

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,9 +7,9 @@ const Header = (props) => {
         <img src={"https://tinyjpg.com/images/social/website.jpg"} alt={""}/>
 
         <div className={s.loginBlock}>
-            {props.isAuth ?
-                props.login :
-                <NavLink to={'/login'}>Login</NavLink>}
+            {props.isAuth
+                ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div>
+                : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
 };
