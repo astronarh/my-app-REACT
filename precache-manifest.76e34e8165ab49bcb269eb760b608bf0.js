0d2777d65434b23f0070e67359c16db7514a9cea
v.shkerdin <v.shkerdin@crpt.ru>
2020-06-15 11:35:30 +0300

    Updates

---
+++ precache-manifest.76e34e8165ab49bcb269eb760b608bf0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6775dfcee3627eed9251646e9b0b06",
+    "url": "/my-app-REACT/index.html"
+  },
+  {
+    "revision": "758ebf67983e6a0500a1",
+    "url": "/my-app-REACT/static/css/3.f8304efa.chunk.css"
+  },
+  {
+    "revision": "b9df273d9c5c8982c72d",
+    "url": "/my-app-REACT/static/css/4.a0e8d7c8.chunk.css"
+  },
+  {
+    "revision": "6a7b954ab778c1d1f6e9",
+    "url": "/my-app-REACT/static/css/main.8a1c0d5b.chunk.css"
+  },
+  {
+    "revision": "71474db3f0e44547170f",
+    "url": "/my-app-REACT/static/js/2.0c2d0401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app-REACT/static/js/2.0c2d0401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758ebf67983e6a0500a1",
+    "url": "/my-app-REACT/static/js/3.5d204fc3.chunk.js"
+  },
+  {
+    "revision": "b9df273d9c5c8982c72d",
+    "url": "/my-app-REACT/static/js/4.eb666403.chunk.js"
+  },
+  {
+    "revision": "6a7b954ab778c1d1f6e9",
+    "url": "/my-app-REACT/static/js/main.0ed55f73.chunk.js"
+  },
+  {
+    "revision": "896bdbea99e8e5c2976d",
+    "url": "/my-app-REACT/static/js/runtime-main.bf4cc247.js"
+  },
+  {
+    "revision": "e003a7066fa07abb625ecb50b2f26648",
+    "url": "/my-app-REACT/static/media/preloader.e003a706.gif"
+  },
+  {
+    "revision": "332173a6ff4fda86c7b4825c2326ea89",
+    "url": "/my-app-REACT/static/media/user.332173a6.jpg"
+  }
+]);
