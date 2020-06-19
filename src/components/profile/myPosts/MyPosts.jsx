f2fc97637191bccf9fb642e0cc56d7cc46f9df17
v.shkerdin <v.shkerdin@crpt.ru>
2020-06-19 18:44:20 +0300

    97

--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -24,7 +24,7 @@ let AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostF
 const MyPosts = React.memo(props => {
     let postsElements = [...props.posts]
                                  .reverse()
-                                 .map(p => <Post message={p.message} likeCount={p.likeCount}/>);
+                                 .map(p => <Post key={p.id} message={p.message} likeCount={p.likeCount}/>);
 
     let newPostElement = React.createRef();
 
